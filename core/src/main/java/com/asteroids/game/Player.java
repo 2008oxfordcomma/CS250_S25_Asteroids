ffa8cd3995760a13124ccb97e9f4d7738adddb85
2008oxfordcomma <2008oxfordcomma@gmail.com>
2025-02-04 11:57:23 -0600

    Added shooting logic

---
+++ core/src/main/java/com/asteroids/game/Player.java
@@ -0,0 +1,53 @@
+package com.asteroids.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class Player {
+    Vector2 position;
+    Vector2 velocity;
+    float angle;
+    float speed = 300;
+    float fireCooldown;
+
+    public Player() {
+        position = new Vector2(400, 300);
+        velocity = new Vector2();
+        angle = 90; // starts pointing straight up
+    }
+
+    public void rotate(float degrees) {
+        angle += degrees;
+    }
+
+    public void thrust() {
+        velocity.x += MathUtils.cosDeg(angle) * speed * Gdx.graphics.getDeltaTime();
+        velocity.y += MathUtils.sinDeg(angle) * speed * Gdx.graphics.getDeltaTime();
+    }
+
+    public void shoot(Array<Bullet> bullets) {
+        if(fireCooldown <= 0) {
+            bullets.add(new Bullet(
+                position.cpy(),
+                new Vector2(MathUtils.cosDeg(angle) * 500, MathUtils.sinDeg(angle) * 500)
+            ));
+            fireCooldown = 0.2f;
+        }
+    }
+
+    public void update(float delta) {
+        position.add(velocity.x * delta, velocity.y * delta);
+        velocity.scl(0.98f); // Friction
+        fireCooldown -= delta;
+    }
+
+    public void draw(ShapeRenderer renderer) {
+        renderer.identity();
+        renderer.translate(position.x, position.y, 0);
+        renderer.rotate(0, 0, 1, angle + 90);
+        renderer.triangle(-10, 10, 0, -15, 10, 10);
+    }
+}
