823a43ce3ced333fdb923ce0e5b3e6fb3041e438
2008oxfordcomma <2008oxfordcomma@gmail.com>
2025-02-04 11:57:39 -0600

    Initial Commit

---
+++ core/src/main/java/com/asteroids/game/Bullet.java
@@ -0,0 +1,37 @@
+package com.asteroids.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class Bullet {
+    Vector2 position;
+    Vector2 velocity;
+    float lifetime = 1.5f;
+
+    public Bullet(Vector2 pos, Vector2 vel) {
+        position = pos;
+        velocity = vel;
+    }
+
+    public void update(float delta) {
+        position.add(velocity.x * delta, velocity.y * delta);
+        lifetime -= delta;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(createWhitePixel(), position.x, position.y, 2, 2);
+    }
+
+    private TextureRegion createWhitePixel() {
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
+        return new TextureRegion(texture);
+    }
+}
