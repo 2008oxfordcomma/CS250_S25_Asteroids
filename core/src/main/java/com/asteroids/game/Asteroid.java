2c947980b0fe071d7c1a74f042f461ceb326dd1e
2008oxfordcomma <2008oxfordcomma@gmail.com>
2025-02-04 12:00:21 -0600

    Initial Commit

---
+++ core/src/main/java/com/asteroids/game/Asteroid.java
@@ -0,0 +1,37 @@
+package com.asteroids.game;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+public class Asteroid {
+    Vector2 position;
+    Vector2 velocity;
+    int size; // 3 = large, 2 = medium, 1 = small
+    float rotation;
+    float rotationSpeed;
+
+    public Asteroid(Vector2 pos, int size) {
+        position = pos;
+        velocity = new Vector2(MathUtils.random(-50, 50), MathUtils.random(-50, 50));
+        this.size = size;
+        rotationSpeed = MathUtils.random(-100, 100);
+    }
+
+    public void update(float delta) {
+        position.add(velocity.x * delta, velocity.y * delta);
+        rotation += rotationSpeed * delta;
+    }
+
+    public void draw(ShapeRenderer renderer) {
+        renderer.identity();
+        renderer.translate(position.x, position.y, 0);
+        renderer.rotate(0, 0, 1, rotation);
+        renderer.circle(0, 0, size * 10, 12);
+    }
+
+    public Circle getBounds() {
+        return new Circle(position.x, position.y, size * 10);
+    }
+}
