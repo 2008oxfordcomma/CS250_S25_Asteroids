d2faee3f96ef39eb503193c66d4e32f2c3a6155d
2008oxfordcomma <2008oxfordcomma@gmail.com>
2025-02-05 20:21:24 -0600

    Added warping and collisions
    Warping for asteroids and bullets added
    Collision between bullet and asteroid added

--- core/src/main/java/com/asteroids/game/AsteroidGame.java
+++ core/src/main/java/com/asteroids/game/AsteroidGame.java
@@ -21,10 +21,12 @@ public class AsteroidGame extends ApplicationAdapter {
     float asteroidSpawnTimer;
 
     void updateBullets(float delta) {
-        for(int i = bullets.size - 1; i >= 0; i--) {
+        for (int i = bullets.size - 1; i >= 0; i--) {
             Bullet bullet = bullets.get(i);
             bullet.update(delta);
-            if(bullet.lifetime <= 0) bullets.removeIndex(i);
+            if (bullet.lifetime <= 0) {
+                bullets.removeIndex(i); // Remove bullet if lifetime is over
+            }
         }
     }
 
@@ -35,15 +37,23 @@ public class AsteroidGame extends ApplicationAdapter {
     }
 
     void checkCollisions() {
-        for(int i = asteroids.size - 1; i >= 0; i--) {
-            Asteroid asteroid = asteroids.get(i);
-            for(Bullet bullet : bullets) {
-                if(asteroid.getBounds().contains(bullet.position)) {
-                    asteroids.removeIndex(i);
-                    if(asteroid.size > 1) {
-                        splitAsteroid(asteroid);
+        // Loop through bullets and asteroids in reverse order to safely remove items
+        for (int i = bullets.size - 1; i >= 0; i--) {
+            Bullet bullet = bullets.get(i);
+            for (int j = asteroids.size - 1; j >= 0; j--) {
+                Asteroid asteroid = asteroids.get(j);
+
+                // Check for collision between bullet and asteroid
+                if (asteroid.getBounds().contains(bullet.position)) {
+                    // Remove the bullet
+                    bullets.removeIndex(i);
+
+                    // Handle asteroid (e.g., split or remove)
+                    if (asteroid.size > 1) {
+                        splitAsteroid(asteroid); // Split into smaller asteroids
                     }
-                    break;
+                    asteroids.removeIndex(j); // Remove the original asteroid
+                    break; // Exit the inner loop since the bullet is destroyed
                 }
             }
         }
@@ -59,17 +69,29 @@ public class AsteroidGame extends ApplicationAdapter {
     }
 
     void wrapAroundScreen() {
+        // Wrap player around the screen
         Vector2 pos = player.position;
-        if(pos.x < 0) pos.x = 800;
-        if(pos.x > 800) pos.x = 0;
-        if(pos.y < 0) pos.y = 600;
-        if(pos.y > 600) pos.y = 0;
+        if (pos.x < 0) pos.x = 800;
+        if (pos.x > 800) pos.x = 0;
+        if (pos.y < 0) pos.y = 600;
+        if (pos.y > 600) pos.y = 0;
 
-        for(Bullet bullet : bullets) {
+        // Wrap asteroids around the screen
+        for (Asteroid asteroid : asteroids) {
+            Vector2 apos = asteroid.position;
+            if (apos.x < 0) apos.x = 800;
+            if (apos.x > 800) apos.x = 0;
+            if (apos.y < 0) apos.y = 600;
+            if (apos.y > 600) apos.y = 0;
+        }
+
+        // Wrap bullets around the screen
+        for (Bullet bullet : bullets) {
             Vector2 bpos = bullet.position;
-            if(bpos.x < 0 || bpos.x > 800 || bpos.y < 0 || bpos.y > 600) {
-                bpos.set(MathUtils.clamp(bpos.x, 0, 800), MathUtils.clamp(bpos.y, 0, 600));
-            }
+            if (bpos.x < 0) bpos.x = 800;
+            if (bpos.x > 800) bpos.x = 0;
+            if (bpos.y < 0) bpos.y = 600;
+            if (bpos.y > 600) bpos.y = 0;
         }
     }
 
@@ -108,11 +130,11 @@ public class AsteroidGame extends ApplicationAdapter {
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         player.draw(shapeRenderer);
-        for(Asteroid asteroid : asteroids) asteroid.draw(shapeRenderer);
+        for (Asteroid asteroid : asteroids) asteroid.draw(shapeRenderer);
         shapeRenderer.end();
 
         batch.begin();
-        for(Bullet bullet : bullets) bullet.draw(batch);
+        for (Bullet bullet : bullets) bullet.draw(batch);
         batch.end();
     }
 
