7a9f895276263eb3dbc27836ff5a241a042650de
2008oxfordcomma <2008oxfordcomma@gmail.com>
2025-02-06 19:56:31 -0600

    Added perlin noise to give randomness to the movements

--- core/src/main/java/com/asteroids/game/Asteroid.java
+++ core/src/main/java/com/asteroids/game/Asteroid.java
@@ -1,3 +1,8 @@
+/**
+ * Author: Dre Harm
+ * Date: 2/4/25
+ */
+
 package com.asteroids.game;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -5,21 +10,34 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
+
 public class Asteroid {
     Vector2 position;
     Vector2 velocity;
     int size; // 3 = large, 2 = medium, 1 = small
     float rotation;
     float rotationSpeed;
+    float noiseTime;
 
     public Asteroid(Vector2 pos, int size) {
         position = pos;
-        velocity = new Vector2(MathUtils.random(-50, 50), MathUtils.random(-50, 50));
         this.size = size;
         rotationSpeed = MathUtils.random(-100, 100);
+        noiseTime = MathUtils.random(1000); // To avoid synchronization, using random start.
+
+        float angle = MathUtils.random(0f, 360f);
+        float speed = MathUtils.random(30, 60);
+        velocity = new Vector2(MathUtils.cosDeg(angle) * speed, MathUtils.sinDeg(angle) * speed);
     }
 
     public void update(float delta) {
+        noiseTime += delta * 0.5f; // Increment time slowly for smoother change
+        float noiseX = PerlinNoise.noise(noiseTime) * 10;
+        float noiseY = PerlinNoise.noise(noiseTime + 100) * 10; // offset gives variation
+
+        velocity.x += noiseX * delta;
+        velocity.y += noiseY * delta;
+
         position.add(velocity.x * delta, velocity.y * delta);
         rotation += rotationSpeed * delta;
     }
@@ -34,6 +52,7 @@ public class Asteroid {
     }
 
     public Circle getBounds() {
+
         return new Circle(position.x, position.y, size * 10);
     }
 }
